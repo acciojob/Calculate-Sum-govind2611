45def1c40732a2ddccb712e24e32b4b158fb088c
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-07-19 08:15:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+
+function getSum(){
+	let n1 = parseInt(document.getElementById("n1").value);
+	let n2 = parseInt(document.getElementById("n2").value);
+	document.getElementById("Sum").innerHTML = String(n1 + n2);
+}
